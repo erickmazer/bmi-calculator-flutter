0c94a152035475937904bd19e7f65c063f2d81b4
Erick Mazer Yamashita <design@erickmazer.com>
2020-07-11 19:27:23 -0300

    Adjusting styles

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
 
 const kBottomContainerHeight = 80.0;
+
 const kAccentColor = Color(0xFFEB1555);
 const kActiveCardColor = Color(0xFF1D1E33);
 const kInactiveCardColor = Color(0xFF111328);
 
-const kLabelTextStyle = TextStyle(fontSize: 16.0, color: Color(0xFF8D8E98));
-const kNumberTextStyle = TextStyle(fontSize: 48.0, fontWeight: FontWeight.w700);
-const kTitleTextStyle = TextStyle(fontSize: 40.0, fontWeight: FontWeight.w700);
+const kLabelTextStyle = TextStyle(
+  fontSize: 16.0,
+  color: Color(0xFF8D8E98),
+);
+
+const kNumberTextStyle = TextStyle(
+  fontSize: 48.0,
+  fontWeight: FontWeight.w700,
+);
+
+const kTitleTextStyle = TextStyle(
+  fontSize: 40.0,
+  fontWeight: FontWeight.w700,
+);
 
 const kLargeButtonTextStyle = TextStyle(
   fontSize: 22.0,
@@ -19,14 +31,15 @@ const kResultTextStyle = TextStyle(
   fontSize: 22.0,
   color: Color(0xFF24D876),
   fontWeight: FontWeight.w700,
+  letterSpacing: 2,
 );
 
 const kBMITextStyle = TextStyle(
   fontSize: 90.0,
-  fontWeight: FontWeight.w700,
+  fontWeight: FontWeight.w800,
 );
 
 const kBodyTextStyle = TextStyle(
-  fontSize: 20.0,
-  fontWeight: FontWeight.w400,
+  fontSize: 22.0,
+  fontWeight: FontWeight.w500,
 );
